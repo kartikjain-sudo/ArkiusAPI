5192cc31f3fc6b85ebacfb2ed2ef517414f49335
Kartik Jain <62670152+kartikjain-sudo@users.noreply.github.com>
2021-03-08 16:33:57 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -6,41 +6,41 @@ function formatted_string(pad, str) {
 
 for(var i=1;i<101;i++) {
   nft_obj[Object.keys(nft_obj).length+1] = {
-    Membership_token: 098789
+    Membership_token: 098789,
     Payment:{
-      Paid: true/false
+      Paid: true,
       Amount: 0000
-    }
-    “Identified”: true/false
-    “Certifier”: true/false
-    “Entity”: true/false:
-    “values”:{
-      “Propose”: “…….”
-      “Modify”: true/false
-      “Vote”: true/false  (vote can be only true, if identified and paid both is true)
-      “Comment/Review”: true/false
-    }
-    “Certifications”:{
-      “Propose”:
-      “Modify”: 
-      “Vote”: 
-      “Comment/Review”:
-    }
-    “Forum Posts”:{
-      “Create”:
-      “Modify”:
-      “Reply”:
-      “Vote” :
-    }
-    “Proposals/Petitions”:
-    “Entity certificate token”:
-    “Champion” : true/false
-    “Budget”:{
-      “Vote”:
-      “modify”:
-      “compensation”
-      “comment/review”:
-      “propose”
+    },
+    Identified: true,
+    Certifier: true,
+    Entity: true,
+    values:{
+      Propose: " ",
+      Modify: true,
+      Vote: true, // (vote can be only true, if identified and paid both is true)
+      Comment: true,
+    },
+    Certifications:{
+      Propose: " ",
+      Modify: " ",
+      Vote: " ", 
+      Comment: " ",
+    },
+    Forum_Posts:{
+      Create: " ",
+      Modify: " ",
+      Reply: " ",
+      Vote: " ",
+    },
+    Proposal: " ",
+    Entity_certificate_token:  " ",
+    Champion : true;
+    Budget:{
+      Vote: " ",
+      modify: " ",
+      compensation: " ",
+      comment: " ",
+      propose: " ",
     }
   }
 }
