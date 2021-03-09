48389b2d99533c04987ec98be22c906c05d617bb
Kartik Jain <62670152+kartikjain-sudo@users.noreply.github.com>
2021-03-09 14:47:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const app = express()
 // Static public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
+app.post('/api/token/:token_id', function(req, res) {
+  const tokenId = parseInt(req.params.token_id).toString()
+
   var metadata = {
     "name": "Arkius Members",
     "description": "Arkius Members are adorable aquatic beings primarily for demonstrating what can be done using the OpenSea platform. Adopt one today to try out all the OpenSea buying, selling, and bidding feature set.",
@@ -22,7 +24,36 @@ app.get('/', function(req, res) {
     "external_link": "https://openseacreatures.io",
     "seller_fee_basis_points": 100, //# Indicates a 1% seller fee.
     "fee_recipient": "0x287A135702555F69BA6eE961f69ee60Fbb87A0e8" //# Where seller fees will be paid to.
-}
+  }
+
+  const Identified = req.body.Identified;
+  const Certifier = req.body.Certifier;
+  const Entity = req.body.Entity;
+  const Champion = req.body.Champion;
+  const MembershipToken = req.body.MembershipToken;
+  const PaymentPaid = req.body.PaymentPaid;
+  const PaymentAmount = req.body.PaymentAmount;
+  const ValuesPropose = req.body.ValuesPropose;
+  const ValuesModify = req.body.ValuesModify;
+  const ValuesVote = req.body.ValuesVote;
+  const ValuesCommentReview = req.body.ValuesCommentReview;
+  const CertificationsPropose = req.body.CertificationsPropose;
+  const CertificationsModify = req.body.CertificationsModify;
+  const CertificationsVote = req.body.CertificationsVote;
+  const CertificationsCommentReview = req.body.CertificationsCommentReview;
+  const ForumPostsCreate = req.body.ForumPostsCreate;
+  const ForumPostsModify = req.body.ForumPostsModify;
+  const ForumPostsVote = req.body.ForumPostsVote;
+  const ForumPostsReply = req.body.ForumPostsReply;
+  const ProposalsPetitions = req.body.ProposalsPetitions;
+  const EntityCertificateToken = req.body.EntityCertificateToken;
+  const BudgetVote = req.body.BudgetVote;
+  const BudgetModify = req.body.BudgetModify;
+  const BudgetCompensation = req.body.BudgetCompensation;
+  const BudgetCommentReview = req.body.BudgetCommentReview;
+  const BudgetPropose = req.body.BudgetPropose;
+
+  console.log(metadata);
   res.send(metadata);
 })
 
