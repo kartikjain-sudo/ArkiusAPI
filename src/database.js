cc2d1187fbdb5316a880da89577c3e3c84e1d026
Kartik Jain <62670152+kartikjain-sudo@users.noreply.github.com>
2021-03-09 17:26:06 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,134 +1,125 @@
-// var nft_obj = {};
+    var nft_obj = {};
 
-// function formatted_string(pad, str) {
-//   return (pad+str).slice(-pad.length);
-// }
-
-// for(var i=1;i<101;i++) {
-//   nft_obj[Object.keys(nft_obj).length+1] = {
-//     Membership_token: 098789,
-//     Payment:{
-//       Paid: true,
-//       Amount: 0000
-//     },
-//     Identified: true,
-//     Certifier: true,
-//     Entity: true,
-//     values:{
-//       Propose: " ",
-//       Modify: true,
-//       Vote: true, // (vote can be only true, if identified and paid both is true)
-//       Comment: true,
-//     },
-//     Certifications:{
-//       Propose: " ",
-//       Modify: " ",
-//       Vote: " ", 
-//       Comment: " ",
-//     },
-//     Forum_Posts:{
-//       Create: " ",
-//       Modify: " ",
-//       Reply: " ",
-//       Vote: " ",
-//     },
-//     Proposal: " ",
-//     Entity_certificate_token:  " ",
-//     Champion : true;
-//     Budget:{
-//       Vote: " ",
-//       modify: " ",
-//       compensation: " ",
-//       comment: " ",
-//       propose: " ",
-//     }
-//   }
-// }
-
-// module.exports = nft_obj;
-// // = {
-
-// //   "1": {
-// //     "name": "Royale Rarities - Queen of Queens (Origins Series)",
-// //     "description": "Origins Series (#001 of 36)",
-// //     "image": "https://s3.amazonaws.com/royale.finance/queen/royaleNFT-queen01.jpg",
-// //     "external_url": "",
-// //     "attributes": [
-// //     ]
-// //   }
-
-// // }
-
-
-var nft_obj = {};
-
-function formatted_string(pad, str) {
-  return (pad + str).slice(-pad.length);
-}
-// for(var i=1;i<4;i++) {
-//   nft_obj[Object.keys(nft_obj).length+1] = {
-    // "name": "MemberApi",
-    // "description": Arkius member,
-    // "image": ``,
-    // "external_url": "",
-    // "attributes": [
-    // ]
-//   }
-// }
-for (var i = 1; i < 100; i++) {
-  nft_obj[Object.keys(nft_obj).length + 1] = {
-    "name": `Member ${i}`,
-    "description": 'Arkius Member',
-    "image": `https://ipfs.io/ipfs/QmPey2czyZyiDMa6MJtoVRFKPHfUHrZawr1ZMoh9AhJU1y?filename=Screenshot_1.png`,
-    "external_url": "",
-    "attributes": [
-    ],
-    "MembershipToken":`Member ${i}`,
-    "Payment": {
-      "Paid": false,
-      "Amount": 0
-    },
-    "Identified": false,
-    "Certifier": false,
-    "Entity": false,
-    "values": {
-      "Propose": '',
-      "Modify": false,
-      "Vote": false,
-      "Comment/Review": false
-    },
-    "Certifications": {
-      "Propose": '',
-      "Modify": '',
-      "Vote": '',
-      "Comment/Review": ''
-    },
-    "ForumPosts": {
-      "Create": '',
-      "Modify": '',
-      "Reply": '',
-      "Vote": ''
-    },
-    "Proposals/Petitions": '',
-    "EntityCertificateToken": '',
-    "Champion": false,
-    "Budget": {
-      "Vote": '',
-      "modify": '',
-      "compensation": '',
-      "comment/review": '',
-      "propose": ''
+    function formatted_string(pad, str) {
+      return (pad + str).slice(-pad.length);
     }
-  }
-}
-module.exports = nft_obj;
-// = {
-//   "1": {
-//     "name": "Royale Rarities - Queen of Queens (Origins Series)",
-//     "description": "Origins Series (#001 of 36)",
-//     "image": "https://s3.amazonaws.com/royale.finance/queen/royaleNFT-queen01.jpg",
-//     "external_url": "",
-//     "attributes": [
-//     ]
-//   }
-// }
+
+    var i = 0;
+
+      nft_obj[Object.keys(nft_obj).length + 1] = {
+
+        function() {
+          i++;
+        },
+        "name": `Member ${i}`,
+        "description": `Arkius Member`,
+        "image": `https://ipfs.io/ipfs/QmPey2czyZyiDMa6MJtoVRFKPHfUHrZawr1ZMoh9AhJU1y?filename=Screenshot_1.png`,
+        "external_url": "",
+        "attributes": [
+          {
+            "trait_type": "Identified",
+            "value": "false"
+          },
+          {
+            "trait_type": "Certifier",
+            "value": "false"
+          },
+          {
+            "trait_type": "Entity",
+            "value": "false"
+          },
+          {
+           "trait_type": "Champion",
+            "value": "true"
+          },
+          {
+            "trait_type": "MembershipToken",
+            "value": `Member ${i}`
+          },
+          {
+            "trait_type": "PaymentPaid",
+            "value": 0,
+          },
+          {
+            "trait_type":"PaymentAmount",
+            "value": 0,
+          },
+          {
+            "trait_type": "ValuesPropose",
+            "value": 1,
+          },
+          {
+            "trait_type": "ValuesModify",
+            "value": 0,
+          },
+          {
+            "trait_type": "ValuesVote",
+            "value": 0,
+          },
+          {
+            "trait_type": "ValuesCommentReview",
+            "value": 0,
+          },
+          {
+            "trait_type": "CertificationsPropose",
+            "value": 1,
+          },
+          {
+            "trait_type": "CertificationsModify",
+            "value": 0,
+          },
+          {
+            "trait_type": "CertificationsVote",
+            "value": 0,
+          },
+          {
+            "trait_type": "CertificationsCommentReview",
+            "value": 0,
+          },
+          {
+            "trait_type": "ForumPostsCreate",
+            "value": 0,
+          },
+          {
+            "trait_type": "ForumPostsModify",
+            "value": 0,
+          },
+          {
+            "trait_type": "ForumPostsVote",
+            "value": 0,
+          },
+          {
+            "trait_type": "ForumPostsReply",
+            "value": 0,
+          },
+          {
+            "trait_type": "ProposalsPetitions",
+            "value": 0,
+          },
+          {
+            "trait_type": "EntityCertificateToken",
+            "value": 0,
+          },
+          {
+            "trait_type": "BudgetVote",
+            "value": 0,
+          },
+          {
+            "trait_type": "BudgetModify",
+            "value": 0,
+          },
+          {
+            "trait_type": "BudgetCompensation",
+            "value": 0,
+          },
+          {
+            "trait_type": "BudgetCommentReview",
+            "value": 0,
+          },
+          {
+            "trait_type": "BudgetPropose",
+            "value": 0,
+          },
+      ],
+    }
+  module.exports = nft_obj;
